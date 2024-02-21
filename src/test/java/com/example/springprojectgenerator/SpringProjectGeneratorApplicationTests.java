e73c998dd4ba1bc0aa43f6ac4f39eb31b3ee5768
zhiradev <zhivko.radeff@gmail.com>
2024-02-21 14:06:22 +0200

    Initial commit

---
+++ src/test/java/com/example/springprojectgenerator/SpringProjectGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springprojectgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
